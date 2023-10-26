91818be1fb6311d00ecf6738da1fe1ce100f51d4
Philmore Foster <88144756+Philmore24@users.noreply.github.com>
2023-10-25 21:30:17 -0400

    Update ex_08_04.py with code works
    Stack-overflow

--- ex_08_04/ex_08_04.py
+++ ex_08_04/ex_08_04.py
@@ -4,3 +4,17 @@ fh = open(fname)
 lst = list()
 for line in fh:
    print(line.rstrip())
+
+
+fname = input("Enter file name: ") 
+fh = open("romeo.txt")
+newlist = list ()
+for line in fh:
+    words = line.split()
+    for word in words:
+        if word not in newlist :
+            newlist.append (word)
+        elif word in newlist :
+            continue
+newlist.sort ()
+print (newlist)
